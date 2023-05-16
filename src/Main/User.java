14bdbc411052ee0a167bc023c44b48d9ff9991f0
Furkan Bulut <furkanbtng@gmail.com>
2023-05-16 12:47:58 +0300

    Project created

---
+++ src/Main/User.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class User {
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private String dateofbirth;
+}
